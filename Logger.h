e586dc78defc4b08118b161ebca6b706b43ce5fb
linus-xuzixuan <linus_xu@live.com>
2018-08-11 22:40:29 +0800

    Adding logid.

--- Logger.h
+++ Logger.h
@@ -6,15 +6,15 @@ By Linus Xu (linus-xuzixuan on GitHub)
 #include <iostream>
 
 using namespace std;
-fstream fin;
+ofstream fout;
 
 void openfile(){
-    fin.open("./LRS.log");
-    if(fin.fail()){
+    fout.open("./LRS.log");
+    if(fout.fail()){
         cout<<"Log opening failed.";
     }
 }
 
 void logid(int numWolverines,int numPowers,int numVillagers){
-
+    fout<<"Total: "<<numWolverines+numPowers+numVillagers<<" players."<<endl;
 }
